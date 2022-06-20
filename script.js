ec4edf12938801c1cea6049b958a550d466c6a26
Isabella <icmlopes@gmail.com>
2022-06-20 18:03:16 -0300

    Selecionando apenas 2 cartas

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let arrayGif = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif']
-let numCartas;
+let numCartas = 10;
 let baralho = document.querySelector(".jogo");
 let cartasDisponiveis = [];
+let cartasViradas = [];
+let cartaSelecionada = [];
+let numClique = 0;
+let maxClique = 2;
 
-enredoJogo()
-
-function iniciarJogo() {
+/* function iniciarJogo() {
   while (numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14) {
     numCartas = Number(prompt("Com quantas cartas você quer jogar? Escolha um nº par entre 4 e 14"));
   }
+}; */
+/* iniciarJogo(); */
+
+
+function sortearCartas() {
+  for (let i = 0; i < numCartas / 2; i++) {
+    cartasDisponiveis.push(arrayGif[i]);
+    cartasDisponiveis.push(arrayGif[i]);
+  }
 };
+sortearCartas();
+cartasDisponiveis.sort(comparador);
 
 for (let i = 0; i < numCartas; i++) {
   baralho.innerHTML += `
@@ -20,15 +33,7 @@ for (let i = 0; i < numCartas; i++) {
 `
 };
 
-function sortearCartas() {
-  for (let i = 0; i < numCartas; i++) {
-    cartasDisponiveis.push(arrayGif[i]);
-  }
-};
-
-// baralho.innerHTML = baralho + baralho; - Ta errado
 arrayGif.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
-
 
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() {
@@ -37,14 +42,34 @@ function comparador() {
 
 
 function girarCarta(elemento) {
+  if (numClique === maxClique){
+    return;
+  }
+
+  cartaSelecionada.push(elemento);
+  numClique++;
   elemento.querySelector(".frente-carta").classList.add("ocultar");
-  elemento.querySelector(".fundo-carta").classList.remove("ocultar")
+  elemento.querySelector(".fundo-carta").classList.remove("ocultar");
+  analisarSelecao();
 };
 
-function enredoJogo() {
-  iniciarJogo();
-  sortearCartas();
-  comparador();
 
+function analisarSelecao() {
+  let carta1=cartaSelecionada[0].innerHTML;
+  let carta2=cartaSelecionada[1].innerHTML;
+  if (carta1===carta2){
+    return;
+  }
 
+};
+
+function numJogadas() {
+
+};
+
+function acabouJogo() {
+  if (cartasViradas === numCartas) {
+    alert(`Você ganhou em ${numJogadas} jogadas! `)
+  }
 }
+
