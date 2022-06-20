71b8ca05b57a2d0f3da9436968f2534b7409f1f5
Isabella <icmlopes@gmail.com>
2022-06-20 10:24:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+
+function iniciarJogo(){
+  let numCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um nº par entre 4 e 14"))
+  const numPar = numCartas % 2;
+  while (numPar !== 0 || numCartas < 4 || numCartas > 14) {
+    numCartas = parseInt(prompt("Com quantas cartas você quer jogar? Escolha um nº par entre 4 e 14"));
+console.log(numCartas)
+  }
+}
+iniciarJogo()
+
+
+//let baralho = document.querySelector(".container-cartas")
+
+
+
+
+//for (let i = 0; i < baralho, i++){
+
+//}
+
+
+//let arrayGif = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif']
+
+
+//arrayGif.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+
+
+// Esta função pode ficar separada do código acima, onde você preferir
+//function comparador() {
+  //  return Math.random() - 0.5;
+//}  
+
+// colocar que é para gerar a quantidade de cartas colocadas?
+
+
+
+//const numInteiro = parseInt(numCartas, 10) - excluir
+
+//let baralho = document.querySelector(".container-cartas");
+
+c
+
+
+//while (numPar !== 0 || numCartas < 4 || numCartas > 14){
+  //  return prompt("Com quantas cartas você quer jogar? Escolha um nº par entre 4 e 14");
+//}
+
+
+
+// let arrayGifs = []
