64002e3cb51877a7773142bcd1e3f2750d4bb3d9
Isabella <icmlopes@gmail.com>
2022-06-23 08:40:35 -0300

    atualização

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let arrayGif = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif']
-let numCartas;
+let numCartas = 4;
 let baralho = document.querySelector(".jogo");
 let cartasDisponiveis = [];
 let cartasViradas = [];
@@ -11,29 +11,36 @@ function iniciarJogo() {
   while (numCartas % 2 !== 0 || numCartas < 4 || numCartas > 14) {
     numCartas = Number(prompt("Com quantas cartas você quer jogar? Escolha um nº par entre 4 e 14"));
   }
+  sortearCartas();
 };
 iniciarJogo();
 
 
 function sortearCartas() {
+  arrayGif.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
   for (let i = 0; i < numCartas / 2; i++) {
     cartasDisponiveis.push(arrayGif[i]);
     cartasDisponiveis.push(arrayGif[i]);
   }
+  cartasDisponiveis.sort(comparador);
+  cartasNoTabuleiro();
 };
-sortearCartas();
-cartasDisponiveis.sort(comparador);
 
+function cartasNoTabuleiro(){
 for (let i = 0; i < numCartas; i++) {
   baralho.innerHTML += `
     <div class="container-cartas" onclick="girarCarta(this)">
-      <img src= "imagens/${cartasDisponiveis[i]}" class="fundo-carta ocultar">
-      <img src= "imagens/front.png" class="frente-carta">
+      <div class="fundo-carta carta">
+        <img src= "imagens/${cartasDisponiveis[i]}">
+      </div>
+      <div class="frente-carta carta">
+        <img src= "imagens/front.png">
+      </div>
     </div>
 `
+}
 };
 
-arrayGif.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() {
@@ -41,27 +48,23 @@ function comparador() {
 }
 
 
+
 function girarCarta(elemento) {
-  if (numClique === maxClique){
+  if(numClique === maxClique){
     return;
   }
-
-  cartaSelecionada.push(elemento);
   numClique++;
-  elemento.querySelector(".frente-carta").classList.add("ocultar");
-  elemento.querySelector(".fundo-carta").classList.remove("ocultar");
-  analisarSelecao();
+  elemento.classList.add("virar");
 };
 
 
-function analisarSelecao() {
-  let carta1=cartaSelecionada[0].innerHTML;
-  let carta2=cartaSelecionada[1].innerHTML;
-  if (carta1===carta2){
+/* function analisarSelecao() {
+  let carta1 = cartaSelecionada[0].innerHTML;
+  let carta2 = cartaSelecionada[1].innerHTML;
+  if (carta1 === carta2) {
     return;
   }
-
-};
+}; */
 
 function numJogadas() {
 
